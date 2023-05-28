a5a5729f9766b7923ced1028443da9edebe89980
Yusuf Kaplan <yusufdenizkaplan@gmail.com>
2023-05-28 17:33:12 +0100

    added search bar to change youtube video/ playlist link, has validation and added styling

---
+++ src/app/media/YouTubePlayer.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import React, { useState } from "react";
+
+const YouTubePlayer: React.FC = () => {
+  const defaultVideoId = "videoseries?list=PLRH7Kv1Vr04TF6cUlx6f-_Vgn3UIVyZFh";
+  const [videoSrc, setVideoSrc] = useState(
+    `https://www.youtube.com/embed/${defaultVideoId}&loop=1&iv_load_policy=3&controls=1&modestbranding=1&playsinline=1&color=white`
+  );
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      const url = new URL(event.target.value);
+      let newVideoId;
+
+      // Ensure that the URL is a YouTube URL
+      if (url.hostname === "www.youtube.com" || url.hostname === "youtube.com") {
+        const vParam = url.searchParams.get("v");
+        const listParam = url.searchParams.get("list");
+
+        if (vParam && listParam) {
+          newVideoId = `${vParam}?list=${listParam}&loop=1&iv_load_policy=3&controls=1&modestbranding=1&playsinline=1&color=white`;
+        } else if (vParam) {
+          newVideoId = `${vParam}?loop=1&iv_load_policy=3&controls=1&modestbranding=1&playsinline=1&color=white`;
+        } else if (listParam) {
+          newVideoId = `videoseries?list=${listParam}&loop=1&iv_load_policy=3&controls=1&modestbranding=1&playsinline=1&color=white`;
+        } else {
+          throw new Error("URL does not contain a video or playlist ID");
+        }
+      } else if (url.hostname === "youtu.be") {
+        newVideoId = url.pathname.slice(1); // remove the leading "/"
+      } else {
+        throw new Error("URL is not a YouTube URL");
+      }
+
+      if (newVideoId) {
+        const newVideoSrc = `https://www.youtube.com/embed/${newVideoId}`;
+        setVideoSrc(newVideoSrc);
+      } else {
+        throw new Error("URL does not contain a video or playlist ID");
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        // Only alert the message if error is an instance of Error
+        alert(`Error: ${error.message}`);
+      } else {
+        // Otherwise, it's an unknown error type and we should handle it accordingly
+        alert("An unknown error occurred");
+      }
+    }
+  };
+
+  return (
+    <div className="w-full h-full flex flex-col items-center">
+      <iframe
+        className="w-full h-fit md:w-fit md:h-full border-0 aspect-video"
+        src={videoSrc}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+      <input
+        type="text"
+        placeholder="Enter YouTube video or playlist URL"
+        className="my-7 p-2 border border-gray-300 dark:bg-black opacity-25 hover:opacity-50 delay-75 duration-300 w-full md:w-1/2"
+        onChange={handleInputChange}
+      />
+    </div>
+  );
+};
+
+export default YouTubePlayer;
--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import YouTubePlayer from "./YouTubePlayer";
 
 const videoId = "videoseries?list=PLRH7Kv1Vr04TF6cUlx6f-_Vgn3UIVyZFh";
 
@@ -8,7 +9,7 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col w-screen h-screen justify-center items-center overflow-y-auto overflow-x-hidden ">
-        <nav className="px-4 py-8 fixed w-full z-20 top-0 left-0 ">
+        <div className="px-4 py-8 fixed w-full z-20 top-0 left-0 ">
           <div className="container flex flex-wrap items-center justify-between mx-auto">
             <a href="/" className="flex group rounded-lg px-5 ">
               <h2 className="mb-3 text-xl font-semibold">
@@ -16,7 +17,7 @@ export default function Home() {
                 Back
               </h2>
             </a>
-            <div className="mb-2 text-3xl font-bold hover:scale-[97%] transition ease-in-out delay-75 duration-300 cursor-default">Embeded Media Viewer</div>
+            <h1 className="mb-2 text-3xl font-bold hover:scale-[97%] transition ease-in-out delay-75 duration-300 cursor-default">Media Viewer</h1>
             <div className="flex group rounded-lg px-5 ">
               <h2 className="mb-3 text-xl font-semibold opacity-0 cursor-default">
                 <span className="inline-block transition-transform group-hover:-translate-x-1 motion-reduce:transform-none mr-1">&lt;-</span>
@@ -24,18 +25,13 @@ export default function Home() {
               </h2>
             </div>
           </div>
-        </nav>
+        </div>
         <div className="my-28 w-full h-full">
           <div className="flex flex-col justify-center items-center w-full h-full p-4 ">
             <div className="w-full h-full flex justify-center items-center ">
-              <iframe
-                className=" w-full h-fit md:w-fit md:h-full border-0 aspect-video"
-                src={videoSrc}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              />
+              <YouTubePlayer />
             </div>
-            <div className="w-full h-fit flex justify-center items-center p-8 ">enter search box or whatever</div>
+            <div className="w-full h-fit flex justify-center items-center p-4 "></div>
           </div>
         </div>
       </div>
