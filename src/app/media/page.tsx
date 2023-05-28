c4e4185c8bfab3d387b814e0eaa3756762296202
Yusuf Kaplan <yusufdenizkaplan@gmail.com>
2023-05-28 04:34:10 +0100

    remake for entire web app using nextjs 13. -added front page styling -added embedded video media page

---
+++ src/app/media/page.tsx
@@ -0,0 +1,44 @@
+import Head from "next/head";
+
+const videoId = "xNRJwmlRBNU";
+
+export default function Home() {
+  const videoSrc = `https://www.youtube.com/embed/${videoId}`;
+
+  return (
+    <>
+      <div className="flex flex-col w-screen h-screen justify-center items-center overflow-y-auto overflow-x-hidden ">
+        <nav className="px-4 py-8 fixed w-full z-20 top-0 left-0 ">
+          <div className="container flex flex-wrap items-center justify-between mx-auto">
+            <a href="/" className="flex group rounded-lg px-5 ">
+              <h2 className="mb-3 text-xl font-semibold">
+                <span className="inline-block transition-transform group-hover:-translate-x-1 motion-reduce:transform-none mr-1">&lt;-</span>
+                Back
+              </h2>
+            </a>
+            <div className="mb-2 text-3xl font-bold hover:scale-[97%] transition ease-in-out delay-75 duration-300 cursor-default">Embeded Media Viewer</div>
+            <div className="flex group rounded-lg px-5 ">
+              <h2 className="mb-3 text-xl font-semibold opacity-0 cursor-default">
+                <span className="inline-block transition-transform group-hover:-translate-x-1 motion-reduce:transform-none mr-1">&lt;-</span>
+                Back
+              </h2>
+            </div>
+          </div>
+        </nav>
+        <div className="my-28 w-full h-full">
+          <div className="flex flex-col justify-center items-center w-full h-full p-4 ">
+            <div className="w-full h-full flex justify-center items-center ">
+              <iframe
+                className=" h-full border-0 aspect-video"
+                src={videoSrc}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            </div>
+            <div className="w-full h-fit flex justify-center items-center p-8 ">enter search box or whatever</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,64 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
+
+      <div className="hover:scale-105 transition ease-in-out delay-75 duration-300 relative items-center justify-center flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-orange-300 after:via-pink-700 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-red-700 before:dark:opacity-10 after:dark:from-green-500 after:dark:via-[#ff8801] after:dark:opacity-40 before:lg:h-[360px]">
+        <div className=" mb-2 text-5xl font-bold hover:scale-[97%] transition ease-in-out delay-75 duration-300 cursor-default">Yusuf Kaplan</div>
+        <div className=" mb-2 text-2xl opacity-50 font-bold hover:scale-[97%] transition ease-in-out delay-75 duration-300 cursor-default">
+          Welcome to my Portfolio
+        </div>
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="/media"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Media <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Check out my embeded media viewer page using YouTube's embed API</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Learn <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Explore the Next.js 13 playground.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Deploy <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
+        </a>
+      </div>
+    </main>
+  );
+}
