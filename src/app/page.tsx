140fb86618dba891aa2a9ba2abbe833e7722640c
Yusuf Kaplan <yusufdenizkaplan@gmail.com>
2023-05-28 04:36:16 +0100

    fixed syntax

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Media <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Check out my embeded media viewer page using YouTube's embed API</p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Check out my embeded media viewer page using YouTube&apos;s embed API</p>
         </a>
 
         <a
