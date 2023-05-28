bcf9b18c26dcdad2cb978b9592f02a32b5668fca
Yusuf Kaplan <yusufdenizkaplan@gmail.com>
2023-05-28 17:59:01 +0100

    changed placeholder

--- src/app/media/YouTubePlayer.tsx
+++ src/app/media/YouTubePlayer.tsx
@@ -61,7 +61,7 @@ const YouTubePlayer: React.FC = () => {
       />
       <input
         type="text"
-        placeholder="Enter YouTube video or playlist URL"
+        placeholder="Paste a URL to a YouTube video or playlist"
         className="my-7 p-2 border border-gray-300 dark:bg-black opacity-25 hover:opacity-50 delay-75 duration-300 w-full md:w-1/2"
         onChange={handleInputChange}
       />
