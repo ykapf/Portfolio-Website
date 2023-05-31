bf0962cba9865b8d321c99d6c022997f2b323c10
Yusuf Kaplan <yusufdenizkaplan@gmail.com>
2023-05-31 16:48:37 +0100

    fixed hover opacity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
 
       <div className="hover:scale-105 transition ease-in-out delay-75 duration-300 relative items-center justify-center flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-orange-300 after:via-pink-700 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-red-700 before:dark:opacity-10 after:dark:from-green-500 after:dark:via-[#ff8801] after:dark:opacity-40 before:lg:h-[360px]">
-        <div className=" mb-2 text-5xl font-bold hover:scale-[97%] transition ease-in-out delay-75 duration-300 cursor-default text-center">Yusuf Kaplan</div>
+        <div className=" mb-2 text-5xl opacity-50 font-bold hover:scale-[97%] transition ease-in-out delay-75 duration-300 cursor-default text-center">
+          Yusuf Kaplan
+        </div>
         <div className=" mb-2 text-2xl opacity-50 font-bold hover:scale-[97%] transition ease-in-out delay-75 duration-300 cursor-default text-center">
           Welcome to my Portfolio
         </div>
